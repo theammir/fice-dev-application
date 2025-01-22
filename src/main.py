07353d2c4aad69a116f48f864da0b7cd4bd8ad7c
theammir <the@ammir.dev>
2025-01-22 03:04:16 +0200

    feat: minimal working example

---
+++ src/main.py
@@ -0,0 +1,43 @@
+import asyncio
+import contextlib
+import logging
+import os
+
+import aiogram
+import dotenv
+from aiogram.client.default import DefaultBotProperties
+from aiogram.dispatcher.dispatcher import Dispatcher, loggers
+from aiogram.enums import ParseMode
+from rich.logging import RichHandler
+
+dotenv.load_dotenv()
+
+
+async def main() -> None:
+    bot_token = os.getenv("BOT_TOKEN")
+    if not bot_token:
+        raise ValueError("expected BOT_TOKEN in env")
+
+    bot = aiogram.Bot(
+        bot_token, default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+
+    dp = Dispatcher()
+
+    await bot.delete_webhook(drop_pending_updates=True)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(message)s",
+        handlers=[
+            RichHandler(rich_tracebacks=True, log_time_format="[%d/%m/%y %H:%M:%S]")
+        ],
+    )
+    loggers.event.setLevel(logging.WARNING)
+
+    with contextlib.suppress(KeyboardInterrupt):
+        asyncio.run(main())
